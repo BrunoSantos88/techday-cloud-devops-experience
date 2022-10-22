4b3dde555bed03a6db8cfa638c6d8e6e5d4363c2
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-22 12:28:57 -0300

    terraform-Rede

--- Build-Terraform-Rede/network-frontend.tf
+++ Build-Terraform-Rede/network-frontend.tf
@@ -1,9 +1,18 @@
-resource "google_compute_subnetwork" "subnet-frontend" {
-  name          = "frontend-vpc-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = "us-central1"
-  network       = google_compute_network.frontend-vpc.id
+resource "google_compute_subnetwork" "private" {
+  name                     = "private"
+  ip_cidr_range            = "10.0.0.0/18"
+  region                   = "us-central1"
+  network                  = google_compute_network.frontend-vpc.id
+  private_ip_google_access = true
 
+  secondary_ip_range {
+    range_name    = "k8s-pod-range"
+    ip_cidr_range = "10.48.0.0/14"
+  }
+  secondary_ip_range {
+    range_name    = "k8s-service-range"
+    ip_cidr_range = "10.52.0.0/20"
+  }
 }
 
 resource "google_compute_network" "frontend-vpc" {
