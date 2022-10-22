2d76c8f5d8a25f306a34f2be759e1044a3588070
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-22 15:28:53 -0300

    terraform-gke

--- Build-GKE-Terraform/node.tf
+++ Build-GKE-Terraform/node.tf
@@ -34,16 +34,6 @@ resource "google_container_node_pool" "node-cluster" {
     preemptible  = true
     disk_size_gb = 10
 
-    service_account = var.service_account
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/trace.append",
-    ]
-
   }
 
 }
