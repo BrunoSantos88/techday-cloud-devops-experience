f6e04b2ea72428dfc3a86fb3f5a236a734ed3631
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-22 14:30:21 -0300

    terraform-gke

--- Build-GKE-Terraform/cluster.tf
+++ Build-GKE-Terraform/cluster.tf
@@ -1,26 +1,16 @@
 #Cluster GKE
 resource "google_container_cluster" "k8_cluster" {
-  project = "bootcamp-363315"
-  region  = "us-central1"
+remove_default_node_pool = true
+initial_node_count       = 1
+network    = "cluster-k8s-vpc"
+subnetwork = "private"
 
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  network    = "cluster-k8s-vpc"
-  subnetwork = "private"
-
-  master_auth {
-    username = "root"
-    password = "mulekao1988"
-
-    client_certificate_config {
-      issue_client_certificate = false
+  
+client_certificate_config {
+issue_client_certificate = false
     }
   }
-}
 
-
-# Node Pool Gerenciado Separadamente
 resource "google_container_node_pool" "nodes_primarios" {
   name       = "${google_container_cluster.k8_cluster.name}-node-pool"
   location   = "us-central1"
