bb002982b8a61c61acc08e72ff10c9cd3847476f
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-22 20:45:40 -0300

    terraform-gke

--- Build-GKE-Terraform/cluster.tf
+++ Build-GKE-Terraform/cluster.tf
@@ -1,7 +1,11 @@
 resource "google_container_cluster" "cluster-k8" {
   name     = "my-cluster-k8"
+  location                 = "us-central1-a"
   remove_default_node_pool = true
-  initial_node_count       = 3
+  initial_node_count       = 1
+  network                  = "cluster-k8s-vpc"
+  subnetwork               = "private"
+
    node_locations = [
     "us-central1-b"
   ]
@@ -14,7 +18,7 @@ resource "google_container_cluster" "cluster-k8" {
   private_cluster_config {
     enable_private_nodes    = true
     enable_private_endpoint = true
-    master_ipv4_cidr_block  = "172.16.0.0/28"
+    master_ipv4_cidr_block  = "10.0.0.0/18"
   }
 
 
