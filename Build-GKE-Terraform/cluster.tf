83aa78f4499e3f7da98e2b09ab9e50609fda5402
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-22 14:47:44 -0300

    terraform-gke

--- Build-GKE-Terraform/cluster.tf
+++ Build-GKE-Terraform/cluster.tf
@@ -34,42 +34,4 @@ resource "google_container_cluster" "k8-cluster" {
     master_ipv4_cidr_block  = "172.16.0.0/28"
   }
 
-resource "google_container_node_pool" "k8s-cluster" {
-  name       = "k8s-cluster"
-  cluster    = "k8-cluster-devops"
-  node_count = 1
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    preemptible  = false
-    machine_type = "e2-small"
-
-  }
-}
-
-resource "google_container_node_pool" "spot" {
-  name    = "spot"
-  cluster = "k8-cluster-devops"
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  autoscaling {
-    min_node_count = 0
-    max_node_count = 10
-  }
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-small"
-    
-  }
-}
-
 }
