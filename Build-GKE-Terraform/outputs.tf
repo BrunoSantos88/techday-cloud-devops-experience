bd95aaca8e6de6581d2515b1d34dd112c8b1874e
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-22 16:39:10 -0300

    terraform-gke

--- Build-GKE-Terraform/outputs.tf
+++ Build-GKE-Terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "kubernetes_cluster_name" {
-  value       = google_container_cluster.k8-cluster.name
+  value       = google_container_cluster.primary.name
   description = "Nome do cluster GKE"
   sensitive   = true
 }
 
 output "kubernetes_cluster_host" {
-  value       = google_container_cluster.k8-cluster.endpoint
+  value       = google_container_cluster.primary.endpoint
   description = "Host do cluster GKE"
   sensitive   = true
 }
