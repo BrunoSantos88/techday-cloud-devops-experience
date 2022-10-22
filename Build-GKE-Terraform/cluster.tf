4ae3aac3e2d962c0e74242f0dd0204d1c3d100a3
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-22 16:37:59 -0300

    terraform-gke

--- Build-GKE-Terraform/cluster.tf
+++ Build-GKE-Terraform/cluster.tf
@@ -1,22 +1,28 @@
-resource "google_container_cluster" "my-gke-cluster" {
+resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
   location = "us-central1"
-  network  = "cluster-k8s-vpc"
-  subnetwork                    = "private"
-  cluster_secondary_range_name  = "k8s-pod-range"
-  services_secondary_range_name = "k8s-service-range"
-  master_ipv4_cidr_block = "10.0.0.0/18"
-  enable_private_nodes = "true"
-  disable_public_endpoint = "true"
 
-  master_authorized_networks_config = [
-    {
-      cidr_blocks = [
-        {
-          cidr_block   = "0.0.0.0/0"
-          display_name = "all-for-testing"
-       },
-      ]
-    },
-  ]
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
