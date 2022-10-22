e563c043cb6eb382089c29f737baf703745b8337
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-22 14:41:30 -0300

    terraform-gke

--- Build-GKE-Terraform/node.tf
+++ Build-GKE-Terraform/node.tf
@@ -1,11 +1,6 @@
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
-resource "google_service_account" "kubernetes" {
-  account_id = "bootcamp-363315"
-}
-
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_node_pool
-resource "google_container_node_pool" "general" {
-  name       = "general"
+resource "google_container_node_pool" "k8s-cluster" {
+  name       = "k8s-cluster"
   cluster    = google_container_cluster.k8-cluster.id
   node_count = 1
 
