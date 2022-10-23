6d78a69a85246e983592512cbc9b0c4c91a19eb9
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-22 21:31:31 -0300

    terraform-gke

--- Build-GKE-Terraform/cluster.tf
+++ Build-GKE-Terraform/cluster.tf
@@ -1,11 +1,14 @@
 resource "google_container_cluster" "cluster-k8" {
   name     = "my-cluster-k8"
-  location                 = "us-central1-a"
   remove_default_node_pool = true
   initial_node_count       = 1
   network                  = "cluster-k8s-vpc"
   subnetwork               = "private"
 
+
+  node_locations = [
+    "us-central1-b", "us-central1-c", "us-central1-a"
+  ]
    
 
   ip_allocation_policy {
--- Build-GKE-Terraform/node.tf
+++ Build-GKE-Terraform/node.tf
@@ -1,9 +1,11 @@
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "my-node-pool"
-  location   = "us-central1"
   cluster    = google_container_cluster.cluster-k8.id
+  node_locations = [
+    "us-central1-b", "us-central1-c", "us-central1-a"
+  ]
   node_count = 3
-
+  min_master_version = "1.22.13-gke.1000"
   node_config {
     preemptible  = true
     machine_type = "e2-medium"
@@ -16,9 +18,21 @@ management {
   }
 
   autoscaling {
-    min_node_count = 0
+    min_node_count = 3
     max_node_count = 10
   }
 
+   max_pods_per_node = 100
+
+   management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    preemptible  = true
+    disk_size_gb = 10
+
+}
 
 }
