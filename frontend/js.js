327f4e1759692ad4a7f1f6dc62538f5efbe8fff2
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-24 14:03:54 -0300

    alterando arquivos deploy

--- frontend/js.js
+++ frontend/js.js
@@ -5,7 +5,7 @@ $("#button-blue").on("click", function() {
     var txt_comentario = $("#comment").val();
 
     $.ajax({
-        url: "http://34.69.8.42",
+        url: "http://127.0.0.0",
         
         type: "post",
         data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
