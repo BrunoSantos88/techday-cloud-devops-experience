fbf97ed8a7b206cb967099e2ce1a099101e0cff2
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-26 16:23:13 -0300

    terraform-gke

--- Build-GKE-Terraform/node.tf
+++ Build-GKE-Terraform/node.tf
@@ -1,8 +1,8 @@
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "my-node-pool"
+resource "google_container_node_pool" "quickstart-dv_nodes" {
+  name       = "my-node-dev"
   location   = "us-central1"
-  cluster    = google_container_cluster.cluster-k8.id
-  node_count = 2
+  cluster    = google_container_cluster.cluster-quickstart-dv.id
+  node_count = 1
 
   node_config {
     preemptible  = true
@@ -16,8 +16,34 @@ management {
   }
 
   autoscaling {
-    min_node_count = 2
-    max_node_count = 10
+    min_node_count = 1
+    max_node_count = 3
+  }
+
+
+}
+
+
+resource "google_container_node_pool" "quickstart-prod_nodes" {
+  name       = "my-node-prod"
+  location   = "us-central1"
+  cluster    = google_container_cluster.quickstart-prod.id
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+  }
+
+management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
   }
 
 
