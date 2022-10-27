76c824176c2c38c21958ed84bebf15f3c229a3c8
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-27 18:33:47 -0300

    gke-deploy

--- build-Gke-devtest/cluster.tf
+++ build-Gke-devtest/cluster.tf
@@ -1,4 +1,4 @@
-resource "google_container_cluster" "cluster-k8-devtest" {
+resource "google_container_cluster" "cluster-k8-dev" {
   name                     = "my-cluster-k8-devtest"
   location                 = "us-central1-a"
   remove_default_node_pool = true
@@ -7,15 +7,9 @@ resource "google_container_cluster" "cluster-k8-devtest" {
   subnetwork               = "private"
   
 
-  ip_allocation_policy {
-    cluster_secondary_range_name  = "k8s-pod-range"
-    services_secondary_range_name = "k8s-service-range"
-  }
-
   private_cluster_config {
     enable_private_nodes    = true
     enable_private_endpoint = false
-    master_ipv4_cidr_block  = "172.16.0.0/28"
   }
 }
 
