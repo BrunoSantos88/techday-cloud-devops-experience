d1a8fcf2ffcbd8cff6f953c4027e820cccc899c2
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-27 13:00:42 -0300

    gke-terraform

--- build-Gke-devtest/cluster.tf
+++ build-Gke-devtest/cluster.tf
@@ -6,21 +6,6 @@ resource "google_container_cluster" "cluster-k8-dev" {
   network                  = "vpc-america-sul-public"
   subnetwork               = "public-subnetwork"
 
-
-  ip_allocation_policy {
-    cluster_primeri_range_name  = "public-subnetwork"
-    
-  }
-  
-  addons_config {
-    http_load_balancing {
-      disabled = true
-    }
-    horizontal_pod_autoscaling {
-      disabled = false
-    }
-  }
-
   private_cluster_config {
     enable_private_nodes    = true
     enable_private_endpoint = false
