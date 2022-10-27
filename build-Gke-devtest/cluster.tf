465b72117d9d1a8712d181e75c4386e193010dfa
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-27 18:38:53 -0300

    gke-deploy

--- build-Gke-devtest/cluster.tf
+++ build-Gke-devtest/cluster.tf
@@ -1,15 +1,29 @@
-resource "google_container_cluster" "cluster-k8-dev" {
+resource "google_container_cluster" "cluster-k8-devtest" {
   name                     = "my-cluster-k8-devtest"
-  location                 = "us-central1-a"
+  location                 = "us-central1-d"
   remove_default_node_pool = true
   initial_node_count       = 1
   network                  = "cluster-k8s-vpc"
   subnetwork               = "private"
   
+  addons_config {
+    http_load_balancing {
+      disabled = true
+    }
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+  }
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "k8s-pod-range"
+    services_secondary_range_name = "k8s-service-range"
+  }
 
   private_cluster_config {
     enable_private_nodes    = true
     enable_private_endpoint = false
+    master_ipv4_cidr_block  = "172.16.0.0/28"
   }
 }
 
--- build-Gke-devtest/node.tf
+++ build-Gke-devtest/node.tf
@@ -1,13 +1,24 @@
-resource "google_container_node_pool" "primary_preemptible_nodes-devtest" {
-  name       = "my-node-devtest"
+resource "google_container_node_pool" "primary_preemptible_nodes-dev-test" {
+  name       = "my-node-pool-devtest"
   location   = "us-central1"
-  cluster    = google_container_cluster.cluster-k8-devtest.name
+  cluster    = google_container_cluster.cluster-k8-devtest.id
   node_count = 1
 
   node_config {
     preemptible  = true
     machine_type = "e2-medium"
 
-}
-
   }
+
+management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 1
+  }
+
+
+}
