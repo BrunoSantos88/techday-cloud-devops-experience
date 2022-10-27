c7c5afc6995b36b029c86709322fac741f81f3f9
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-27 18:40:35 -0300

    gke-deploy

--- build-Gke-devtest/cluster.tf
+++ build-Gke-devtest/cluster.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "cluster-k8-devtest" {
   name                     = "my-cluster-k8-devtest"
-  location                 = "us-central1-d"
+  location                 = "us-central1-f"
   remove_default_node_pool = true
   initial_node_count       = 1
   network                  = "cluster-k8s-vpc"
