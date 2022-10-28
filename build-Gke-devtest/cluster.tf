566cb82af998315c9374c817f2487419997a3065
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-28 00:39:26 -0300

    gke-deploy

--- build-Gke-devtest/cluster.tf
+++ build-Gke-devtest/cluster.tf
@@ -1,15 +1,17 @@
 resource "google_container_cluster" "cluster-k8-dev-test" {
   name                     = "my-cluster-k8-dev-test"
   location                 = "us-central1-f"
-  enable_autopilot         = true
+  remove_default_node_pool = true
   initial_node_count       = 1
   network                  = "cluster-k8s-vpc"
   subnetwork               = "private"
 
-  
-  
   private_cluster_config {
     enable_private_nodes    = false
     enable_private_endpoint = false
+    
   }
+
+  enable_autopilot = true
 }
+
