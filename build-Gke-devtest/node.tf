23c249c01d28c491e00bf44a60840014c026e897
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-28 00:24:51 -0300

    gke-deploy

--- build-Gke-devtest/node.tf
+++ build-Gke-devtest/node.tf
@@ -1,7 +1,32 @@
 resource "google_container_node_pool" "primary_preemptible_nodes-dev-test" {
-  name       = "my-node-pool-devtest"
+  name       = "my-node-pool-dev-test"
   location   = "us-central1"
-  cluster    = google_container_cluster.cluster-k8-devtest.id
+  cluster    = google_container_cluster.cluster-k8-dev-test.id
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+  }
+
+management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 1
+  }
+
+
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes-staging-test" {
+  name       = "my-node-pool-staging"
+  location   = "us-central1"
+  cluster    = google_container_cluster.cluster-k8-staging-test.id
   node_count = 1
 
   node_config {
--- build-Gke-devtest/outputs.tf
+++ build-Gke-devtest/outputs.tf
@@ -1,11 +1,23 @@
 output "kubernetes_cluster_name" {
-  value       = google_container_cluster.cluster-k8-devtest.name
+  value       = google_container_cluster.cluster-k8-dev-test.name
   description = "Nome do cluster GKE"
   sensitive   = false
 }
 
 output "kubernetes_cluster_host" {
-  value       = google_container_cluster.cluster-k8-devtest.endpoint
+  value       = google_container_cluster.cluster-k8-dev-test.endpoint
+  description = "Host do cluster GKE"
+  sensitive   = false
+}
+
+output "kubernetes_cluster_name" {
+  value       = google_container_cluster.cluster-k8-staging-test.name
+  description = "Nome do cluster GKE"
+  sensitive   = false
+}
+
+output "kubernetes_cluster_host" {
+  value       = google_container_cluster.cluster-k8-staging-test.endpoint
   description = "Host do cluster GKE"
   sensitive   = false
 }
