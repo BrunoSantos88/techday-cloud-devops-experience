bda4b3c3f9469e18065770fe78f0801cec52499e
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-28 00:28:28 -0300

    gke-deploy

--- build-Gke-devtest/outputs.tf
+++ build-Gke-devtest/outputs.tf
@@ -1,22 +1,22 @@
-output "kubernetes_cluster_name" {
+output "kubernetes_cluster_name-1" {
   value       = google_container_cluster.cluster-k8-dev-test.name
   description = "Nome do cluster GKE"
   sensitive   = false
 }
 
-output "kubernetes_cluster_host" {
+output "kubernetes_cluster_host-1" {
   value       = google_container_cluster.cluster-k8-dev-test.endpoint
   description = "Host do cluster GKE"
   sensitive   = false
 }
 
-output "kubernetes_cluster_name" {
+output "kubernetes_cluster_name-2" {
   value       = google_container_cluster.cluster-k8-staging-test.name
   description = "Nome do cluster GKE"
   sensitive   = false
 }
 
-output "kubernetes_cluster_host" {
+output "kubernetes_cluster_host-2" {
   value       = google_container_cluster.cluster-k8-staging-test.endpoint
   description = "Host do cluster GKE"
   sensitive   = false
