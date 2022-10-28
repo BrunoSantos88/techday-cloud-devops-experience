27d5f62893d2af251061380904eb99e2b663cafb
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-10-28 10:56:27 -0300

    gke-deploy

--- GKE-Prod/outputs.tf
+++ GKE-Prod/outputs.tf
@@ -1,11 +1,11 @@
 output "kubernetes_cluster_name" {
-  value       = google_container_cluster.cluster-k8.name
+  value       = google_container_cluster.cluster-k8-produção.name
   description = "Nome do cluster GKE"
   sensitive   = false
 }
 
 output "kubernetes_cluster_host" {
-  value       = google_container_cluster.cluster-k8.endpoint
+  value       = google_container_cluster.cluster-k8-produção.endpoint
   description = "Host do cluster GKE"
   sensitive   = false
 }
