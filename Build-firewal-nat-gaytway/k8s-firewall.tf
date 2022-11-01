b98acac8e84fe0348143a43932af40e20c2b6475
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-11-01 11:22:31 -0300

    alterando cloudbuild

--- Build-firewal-nat-gaytway/k8s-firewall.tf
+++ Build-firewal-nat-gaytway/k8s-firewall.tf
@@ -1,6 +1,6 @@
 resource "google_compute_firewall" "k8s-cluster" {
   project = "bootcamp-363315"
-  name    = "cluster-k8-firewall"
+  name    = "cluster-k8-firewall-devtest"
   network = "cluster-k8s-vpc"
   allow {
     protocol = "icmp"
@@ -8,7 +8,7 @@ resource "google_compute_firewall" "k8s-cluster" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22","80","8080","443","30000-32900"]
+    ports    = ["22","80","8080","443","30000-32900","30005"]
   }
 
   source_tags   = ["cluster-k8s"]
--- Build-firewall-IGW/k8s-firewall.tf
+++ Build-firewall-IGW/k8s-firewall.tf
@@ -1,6 +1,6 @@
-resource "google_compute_firewall" "k8s-cluster-devtest" {
+resource "google_compute_firewall" "k8s-cluster" {
   project = "bootcamp-363315"
-  name    = "cluster-k8-gw-devtest"
+  name    = "cluster-k8-gw-public"
   network = "cluster-k8s-vpc"
   allow {
     protocol = "icmp"
@@ -8,10 +8,10 @@ resource "google_compute_firewall" "k8s-cluster-devtest" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22","80","8080","443","30000-32900","3306","30005"]
+    ports    = ["22","80","8080","443","30000-32900","3306"]
   }
 
-  source_tags   = ["cluster-k8-devetest"]
+  source_tags   = ["cluster-k8-dev"]
   source_ranges = ["0.0.0.0/0"]
 
 }
