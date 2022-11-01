753eb71bed76d8cda3a0c510bed5239770b06a39
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-11-01 11:19:02 -0300

    alterando cloudbuild

--- Build-firewall-IGW/k8s-firewall.tf
+++ Build-firewall-IGW/k8s-firewall.tf
@@ -1,6 +1,6 @@
-resource "google_compute_firewall" "k8s-cluster" {
+resource "google_compute_firewall" "k8s-cluster-devtest" {
   project = "bootcamp-363315"
-  name    = "cluster-k8-gw-public"
+  name    = "cluster-k8-gw-devtest"
   network = "cluster-k8s-vpc"
   allow {
     protocol = "icmp"
@@ -8,10 +8,10 @@ resource "google_compute_firewall" "k8s-cluster" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22","80","8080","443","30000-32900","3306"]
+    ports    = ["22","80","8080","443","30000-32900","3306","30005"]
   }
 
-  source_tags   = ["cluster-k8-dev"]
+  source_tags   = ["cluster-k8-devetest"]
   source_ranges = ["0.0.0.0/0"]
 
 }
