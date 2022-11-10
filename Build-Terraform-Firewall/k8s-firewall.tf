ab7c88b0157f3ec86223a5771dab2dc84093a8b6
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-11-10 18:23:30 -0300

    gatilhodocker

---
+++ Build-Terraform-Firewall/k8s-firewall.tf
@@ -0,0 +1,17 @@
+resource "google_compute_firewall" "k8s-cluster" {
+  project = "bootcamp-363315"
+  name    = "cluster-k8-firewall-devtest"
+  network = "cluster-k8s-vpc"
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22","80","8080","443","30000-32900","30005"]
+  }
+
+  source_tags   = ["cluster-k8s"]
+  source_ranges = ["0.0.0.0/0"]
+
+}
