5825c77ea2bbbf96c47a9ae39b6dffbed3932370
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-11-12 14:42:40 -0300

    pipeline

--- outputs/gke-outpust.tf
+++ outputs/gke-outpust.tf
@@ -1,34 +1,39 @@
-output "name" {
+output "Cluster Dev-Test" {
   # This may seem redundant with the `name` input, but it serves an important
   # purpose. Terraform won't establish a dependency graph without this to interpolate on.
   description = "The name of the cluster master. This output is used for interpolation with node pools, other modules."
 
-  value = google_container_cluster.cluster.name
-}
-
-output "master_version" {
-  description = "The Kubernetes master version."
-  value       = google_container_cluster.cluster.master_version
+  value = var.resourse_group_name_cluster_dev_test
 }
 
 output "endpoint" {
   description = "The IP address of the cluster master."
   sensitive   = true
-  value       = google_container_cluster.cluster.endpoint
+  value       = var.resourse_group_name_cluster_dev_test
 }
 
-# The following outputs allow authentication and connectivity to the GKE Cluster.
-output "client_certificate" {
-  description = "Public certificate used by clients to authenticate to the cluster endpoint."
-  value       = base64decode(google_container_cluster.cluster.master_auth[0].client_certificate)
+output "kubernetes_cluster_host" {
+  value       = var.resourse_group_name_cluster_dev_test
+  description = "Host do cluster GKE"
+  sensitive   = false
 }
 
-output "client_key" {
-  description = "Private key used by clients to authenticate to the cluster endpoint."
-  value       = base64decode(google_container_cluster.cluster.master_auth[0].client_key)
+output "Cluster Prod" {
+  # This may seem redundant with the `name` input, but it serves an important
+  # purpose. Terraform won't establish a dependency graph without this to interpolate on.
+  description = "The name of the cluster master. This output is used for interpolation with node pools, other modules."
+
+  value = var.resourse_group_name_cluster_dev_test
 }
 
-output "cluster_ca_certificate" {
-  description = "The public certificate that is the root of trust for the cluster."
-  value       = base64decode(google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
+output "endpoint" {
+  description = "The IP address of the cluster master."
+  sensitive   = true
+  value       = var.resourse_group_name_cluster_prod
+}
+
+output "kubernetes_cluster_host" {
+  value       = var.resourse_group_name_cluster_prod
+  description = "Host do cluster GKE"
+  sensitive   = false
 }
---
+++ outputs/variavel.tf
@@ -0,0 +1,11 @@
+variable "resourse_group_name_cluster_dev_test" {
+  default     = "my-cluster-k8-dev-test"
+  description = "cluster Gke dev-teste"
+  #GKE devtest
+}
+
+variable "resourse_group_name_cluster_prod" {
+  default     = "my-cluster-k8"
+  description = "cluster Gke Prod"
+  #GKE devtest
+}
