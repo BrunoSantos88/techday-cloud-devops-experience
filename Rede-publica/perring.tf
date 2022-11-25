337181b10881f0ec46830e81fd90670268622bc0
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-11-25 16:53:30 -0300

    rede

--- Rede-publica/perring.tf
+++ Rede-publica/perring.tf
@@ -1,12 +1,12 @@
 resource "google_compute_network_peering" "peering1" {
   name         = "peering1"
   network      = google_compute_network.vpc1.id
-  peer_network = var.resource_rede_clusterk8
+  peer_network = "cluster-k8s-vpc"
 }
 
 
 resource "google_compute_network_peering" "peering2" {
   name         = "peering2"
-  network      = var.resource_rede_clusterk8
+  network      = "cluster-k8s-vpc"
   peer_network = google_compute_network.vpc1.id
 }
