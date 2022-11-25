43b35e47d2dfc650c5be2221d005903999f1e54b
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-11-24 23:12:32 -0300

    bancodados

--- Build-database/sql.tf
+++ Build-database/sql.tf
@@ -7,10 +7,32 @@ resource "google_sql_database" "database" {
 resource "google_sql_database_instance" "instance" {
   name             = "my-database-instance"
   region           = "us-central1"
-  database_version = "MYSQL_8_0"
-  settings {
+  database_version = "MYSQL_5_7"
+  settings{
     tier = "db-f1-micro"
+    user_labels = {
+      "environment" = "development"
+    }
   }
 
-  deletion_protection  = "true"
+
+ip_configuration {
+      ipv4_enabled = false
+      # private_network = var.vpc_name
+      private_network = "cluster-k8s-vpc"
+    }
+  
+
+  deletion_protection  = "false"
+}
+
+resource "google_sql_user" "users" {
+name = "root"
+instance = "${google_sql_database_instance.instance.name}"
+host = "%"
+password = "mypassw0rd"
+}
+
+output "connection_name" {
+  value = google_sql_database_instance.instance.connection_name
 }
