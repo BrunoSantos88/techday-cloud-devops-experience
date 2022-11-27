8d964e1c098374faaeef3f2520c5751cbe36b299
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-11-27 15:31:26 -0300

    backendupdate

--- Docker-Projetos/frontend/js.js
+++ Docker-Projetos/frontend/js.js
@@ -5,7 +5,7 @@ $("#button-blue").on("click", function() {
     var txt_comentario = $("#comment").val();
 
     $.ajax({
-        url: "http://35.247.244.189",
+        url: "http://http://35.199.99.28/",
         
         type: "post",
         data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
