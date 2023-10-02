5b4e01aa4d5d7075c25e4daac970b05bd1befa41
BrunoSantos88 <brunosantos88>
2023-10-02 16:56:41 -0300

    nomesarquivos

--- providers.tf
+++ providers.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = "1.3.7"
 }
 
 provider "google" {
-  project = "qwiklabs-gcp-04-15179ed14b7b"
+  project = "devops-399217"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
