59a13e5fe916d006adf996a647a1f4c1a7e3bfca
BrunoSantos88 <brunosantos88>
2023-10-02 18:01:26 -0300

    bulldspec

--- devops-network/jenkins.tf
+++ devops-network/jenkins.tf
@@ -7,7 +7,7 @@ resource "google_compute_instance" "my_instance" {
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
-      size =  100
+      size =  250
     }
   }
   network_interface {
