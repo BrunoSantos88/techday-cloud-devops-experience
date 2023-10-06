df884701dceba3dc61b748e5e6f7266ae1a6f6d4
BrunoSantos88 <brunosantos88>
2023-10-06 14:54:45 -0300

    bulldspec

--- devops-network/ansible.tf
+++ devops-network/ansible.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "ansible_instance" {
   name         = "Ansible"
   machine_type = "e2-medium"
-  zone         = "us-east-a"
+  zone         = "us-east1-b"
 
   boot_disk {
     initialize_params {
