1cf43ddcdd25aa7f0ffcf4a89784a2c7b6af61b8
BrunoSantos88 <brunosantos88>
2023-10-06 14:49:19 -0300

    bulldspec

--- devops-network/ansible.tf
+++ devops-network/ansible.tf
@@ -15,4 +15,15 @@ resource "google_compute_instance" "ansible_instance" {
        # Associe um endereço IP público padrão à instância
     access_config {}
   }
+
+   metadata_startup_script = <<EOF
+#!/bin/bash
+ sudo apt-get update
+ sudo apt-get install python -y
+ sudo apt install ansible -y
+EOF
+}
+
+output "instance_ip" {
+  value = google_compute_instance.ansible_instance.network_interface[0].access_config[0].nat_ip
 }
