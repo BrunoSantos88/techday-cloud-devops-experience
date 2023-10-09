e16463c5d3dfb5a8b0fdb9cf453bc4638ab75706
BrunoSantos88 <brunosantos88>
2023-10-09 17:21:19 -0300

    tfiles

--- devops-network/node.tf
+++ devops-network/node.tf
@@ -10,14 +10,14 @@ resource "google_compute_instance" "node_instance" {
     }
   }
 
-    metadata_startup_script = base64encode(file("node.sh"))
-
   network_interface {
     network = "default"
     
        # Associe um endereço IP público padrão à instância
     access_config {}
   }
+  
+  metadata_startup_script = base64encode(file("node.sh"))
 
   }
 
