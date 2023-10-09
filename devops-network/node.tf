4ae4b58c172a8f499e9717d35a9beee905ef31a1
BrunoSantos88 <brunosantos88>
2023-10-09 15:30:42 -0300

    tfiles

--- devops-network/node.tf
+++ devops-network/node.tf
@@ -1,17 +1,17 @@
-
-resource "google_compute_instance" "jenkins_instance" {
-  name         = "jenkins-server"
+resource "google_compute_instance" "node_instance" {
+  name         = "node-agent"
   machine_type = "e2-medium"
-  zone         = "us-central1-a"
+  zone         = "us-east1-b"
 
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
-      size =  250
+      size =  80
     }
   }
 
-  metadata_startup_script = base64encode(file("jenkins.sh"))
+  metadata_startup_script = base64encode(file("node.sh"))
+
 
   network_interface {
     network = "default"
@@ -19,5 +19,4 @@ resource "google_compute_instance" "jenkins_instance" {
        # Associe um endereço IP público padrão à instância
     access_config {}
   }
-
 }
