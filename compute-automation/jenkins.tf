61b112e3be81a6db9d4bb66567f06518a1d8a91c
BrunoSantos88 <brunosantos88>
2023-10-10 20:44:52 -0300

    jenkins

--- compute-automation/jenkins.tf
+++ compute-automation/jenkins.tf
@@ -24,3 +24,20 @@ resource "google_compute_instance" "jenkins_instance" {
 
     metadata_startup_script = data.template_file.startup_script.rendered
 }
+
+resource "google_compute_firewall" "allow-instance" {
+  name          = "allow2-instance"
+  network       = "default"  # Change to your network if needed
+  priority      = 1000       # Adjust the priority as needed
+
+  source_ranges = ["0.0.0.0/0"]  # Be cautious with this; restrict the source IP range if possible
+
+  # Define the allowed ports
+  allow {
+    protocol = "tcp"
+    ports    = ["8080", "22"]  # Example ports (customize as needed)
+  }
+
+  # Specify the target tags to associate this rule with instances
+  target_tags   = ["jenkins-instance"]  # Replace with your instance's tag
+}
