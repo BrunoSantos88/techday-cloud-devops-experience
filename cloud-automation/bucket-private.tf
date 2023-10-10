0aee3664134a52b0335a85546f14590645a599f6
BrunoSantos88 <brunosantos88>
2023-10-10 10:58:56 -0300

    tffilesgke

--- cloud-automation/bucket-private.tf
+++ cloud-automation/bucket-private.tf
@@ -3,3 +3,12 @@ resource "google_storage_bucket" "private_bucket" {
   location      = "US"                                          
   storage_class = "STANDARD"                                     
 }
+
+resource "google_storage_bucket_iam_binding" "example_binding" {
+  bucket = google_storage_bucket.private_bucket.name
+  role   = "roles/storage.objectViewer"  # Essa é a função que concede a permissão 'storage.objects.list'
+
+  members = [
+    "user:brunosantosc1@gmail.com",  # Substitua pelo identificador da entidade a ser concedida a permissão
+  ]
+}
--- cloud-automation/bucket-public.tf
+++ cloud-automation/bucket-public.tf
@@ -5,3 +5,12 @@ resource "google_storage_bucket" "example_bucket" {
   # Enable uniform bucket-level access
   uniform_bucket_level_access = true
 }
+
+resource "google_storage_bucket_iam_binding" "example_binding" {
+  bucket = google_storage_bucket.example_bucket.name
+  role   = "roles/storage.objectViewer"  # Essa é a função que concede a permissão 'storage.objects.list'
+
+  members = [
+    "user:brunosantosc1@gmail.com",  # Substitua pelo identificador da entidade a ser concedida a permissão
+  ]
+}
