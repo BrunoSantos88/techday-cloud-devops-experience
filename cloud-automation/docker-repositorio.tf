ec0de2d38fcc5c9d258536b0263cce310f19828b
BrunoSantos88 <brunosantos88>
2023-10-10 00:29:23 -0300

    tfiles

--- cloud-automation/docker-repositorio.tf
+++ cloud-automation/docker-repositorio.tf
@@ -1,6 +1,17 @@
-resource "google_artifact_registry_repository" "my-repo" {
+resource "google_artifact_registry_repository" "my-frontend" {
   location      = "us-central1"
-  repository_id = "my-repository"
+  repository_id = "my-frontend"
+  description   = "example docker repository"
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = true
+  }
+}
+
+resource "google_artifact_registry_repository" "my-backend" {
+  location      = "us-central1"
+  repository_id = "my-backend"
   description   = "example docker repository"
   format        = "DOCKER"
 
