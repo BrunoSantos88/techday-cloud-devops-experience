e020602bc676dc6a5186e84375b981bb20c29eba
BrunoSantos88 <brunosantos88>
2023-10-10 16:56:26 -0300

    tfiles

--- compute-automation/disck.tf
+++ compute-automation/disck.tf
@@ -1,6 +1,6 @@
 resource "google_compute_disk" "my_disk" {
-  name  = "my-disk"
-  size  = 250  # Size of the disk in GB
+  name  = "jekins-disk"
+  size  = 500  # Size of the disk in GB
   type  = "pd-standard"  # Type of disk (pd-standard or pd-ssd)
   zone  = "us-central1-a"  # Specify the desired zone
 }
