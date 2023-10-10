218d68c1d1867e1f3b22d4c327f3a395a2b94952
BrunoSantos88 <brunosantos88>
2023-10-09 22:43:52 -0300

    tffiles

--- cloud-automation/bucket-public.tf
+++ cloud-automation/bucket-public.tf
@@ -1,35 +1,7 @@
-resource "google_storage_bucket" "public_bucket" {
-  name     = "publicbucketmentoria"
-  location = "US"
+resource "google_storage_bucket" "example_bucket" {
+  name     = "mentoriabucketpublic"
+  location = "US" # Change to your desired location
+
+  # Enable uniform bucket-level access
+  uniform_bucket_level_access = true
 }
-
-resource "google_storage_bucket_iam_binding" "example_binding" {
-  bucket = google_storage_bucket.public_bucket.id
-  role   = "roles/storage.objectViewer"
-
-  members = ["allUsers"]
-}
-
-resource "google_storage_bucket_iam_condition" "example_condition" {
-  bucket = google_storage_bucket.public_bucket.id
-
-  title       = "ip-allowlist-and-time-window"
-  description = "Allow access from specific IP address within a time window"
-  expression  = "request.origin.ip == '187.10.145.102' && timestamp.now >= '2023-10-09T08:00:00Z' && timestamp.now <= '2023-10-09T16:00:00Z'"
-}
-
-resource "google_storage_bucket_object_lifecycle_rule" "example_rule" {
-  bucket = google_storage_bucket.public_bucket.id
-
-  action {
-    type = "Delete"
-  }
-
-  condition {
-    age_days = 1
-  }
-}
-
-
-
-
