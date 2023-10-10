155e9d7a06070092e8bdf012f29785904c25dd06
BrunoSantos88 <brunosantos88>
2023-10-10 10:20:01 -0300

    tffilesgke

--- cloud-automation/gke-aplication.tf
+++ cloud-automation/gke-aplication.tf
@@ -7,6 +7,16 @@ resource "google_container_cluster" "aplication-cluster" {
   }
 }
 
+resource "local_file" "kube_config" {
+  filename = "${path.module}/kube_config"
+  content  = google_container_cluster.aplication-cluster.master_auth[0].kubeconfig
+}
+
+resource "google_storage_bucket_object" "kube_config" {
+  name   = "kube_config"
+  bucket = google_storage_bucket.private_bucket.name
+  source = local_file.kube_config.filename
+}
 resource "google_compute_firewall" "gkeaplication_firewall" {
   name    = "gkeaplication-cluster-firewall"
   network = "default"  # You can change this to the desired network name
--- cloud-automation/gke-services.tf
+++ cloud-automation/gke-services.tf
@@ -7,6 +7,17 @@ resource "google_container_cluster" "services-cluster" {
   }
 }
 
+
+resource "local_file" "kube_config" {
+  filename = "${path.module}/kube_config"
+  content  = google_container_cluster.services-cluster.master_auth[0].kubeconfig
+}
+
+resource "google_storage_bucket_object" "kube_config" {
+  name   = "kube_config"
+  bucket = google_storage_bucket.private_bucket.name
+  source = local_file.kube_config.filename
+}
 resource "google_compute_firewall" "gkeservice_firewall" {
   name    = "gkeservice-cluster-firewall"
   network = "default"  # You can change this to the desired network name
