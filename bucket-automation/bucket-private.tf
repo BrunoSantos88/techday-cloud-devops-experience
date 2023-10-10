5e4f409964a3cdc1e415d38df0ba892a6c7f9888
BrunoSantos88 <brunosantos88>
2023-10-10 13:15:42 -0300

    tfiles

--- bucket-automation/bucket-private.tf
+++ bucket-automation/bucket-private.tf
@@ -3,15 +3,3 @@ resource "google_storage_bucket" "private_bucket" {
   location      = "US"                                          
   storage_class = "STANDARD"                                     
 }
-
-resource "google_storage_bucket_iam_binding" "example_binding" {
-  bucket = google_storage_bucket.private_bucket.name
-  role   = "roles/storage.objectViewer"  # Essa é a função que concede a permissão 'storage.objects.list'
-
-
-  members = [
-    "user:devops-399217@appspot.gserviceaccount.com",  
-     "user:brunosantosc1@gmail.com",
-     "user:devops-399217@appspot.gserviceaccount.com",
-  ]
-}
