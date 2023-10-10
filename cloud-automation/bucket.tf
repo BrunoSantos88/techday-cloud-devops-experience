e89b6edf7ff5a4ef3a896c4a32a2193e76999b02
BrunoSantos88 <brunosantos88>
2023-10-09 22:20:52 -0300

    tffiles

--- cloud-automation/bucket.tf
+++ cloud-automation/bucket.tf
@@ -4,10 +4,6 @@ resource "google_storage_bucket" "private_bucket" {
   storage_class = "STANDARD"                                     
 }
 
-output "bucket_url" {
-  value = "gs://${google_storage_bucket.private_bucket.id}"
-}
-
 resource "google_storage_bucket" "public_bucket" {
   name          = "devopsmentoriapublic"
   location      = "US" 
@@ -24,8 +20,4 @@ resource "google_storage_bucket_iam_binding" "ip_acl" {
   members = [
     "user:187.10.145.102",
   ]
-}
-
-output "bucket_url" {
-  value = "gs://${google_storage_bucket.public_bucket.id}"
 }
