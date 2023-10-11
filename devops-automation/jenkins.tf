a489042ef64d3983ce1d9f32ddd097395b20ca75
BrunoSantos88 <brunosantos88>
2023-10-11 09:32:10 -0300

    devops

---
+++ devops-automation/jenkins.tf
@@ -0,0 +1,26 @@
+data "template_file" "startup_script" {
+  template = file("jenkins.sh")
+}
+
+resource "google_compute_instance" "jenkins_instance" {
+  name         = "jenkins-instance"
+  machine_type = "e2-standard-2"
+  zone         = "us-central1-a"
+
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+      size          = 500
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  tags = ["jenkins-instance"]
+
+    metadata_startup_script = data.template_file.startup_script.rendered
+}
