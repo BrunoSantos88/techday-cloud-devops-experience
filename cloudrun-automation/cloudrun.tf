d43007fa146d16d29b601232175ef7f014ffd972
BrunoSantos88 <brunosantos88>
2023-10-12 20:57:00 -0300

    cloudrun

--- cloudrun-automation/cloudrun.tf
+++ cloudrun-automation/cloudrun.tf
@@ -11,8 +11,8 @@ resource "google_cloud_run_service" "rancher" {
         }
         resources {
           limits = {
-            cpu    = "4.0"   # 0.5 núcleos de CPU
-            memory = "2048"  # 512 MiB de memória
+            cpu    = "2.0"   # 0.5 núcleos de CPU
+            memory = "1024"  # 512 MiB de memória
           }
         }
       }
